0ad78c879b03205a30b110ca38cc621141e82868
Duncan Watson <duncan@hiveit.co.uk>
2020-11-23 11:25:52 +0000

    EES-1618 - swapped out singleton wiring for transient for new ProcessorService

--- src/GovUk.Education.ExploreEducationStatistics.Data.Processor/Services/Interfaces/IProcessorService.cs
+++ src/GovUk.Education.ExploreEducationStatistics.Data.Processor/Services/Interfaces/IProcessorService.cs
@@ -1,6 +1,5 @@
 using System.Threading.Tasks;
 using GovUk.Education.ExploreEducationStatistics.Data.Processor.Model;
-using GovUk.Education.ExploreEducationStatistics.Data.Processor.Models;
 using Microsoft.Azure.WebJobs;
 
 namespace GovUk.Education.ExploreEducationStatistics.Data.Processor.Services.Interfaces
